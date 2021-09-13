10e2c1b117ef14f004c717b296223cef3bd37e8d
Your Name <forrest.mccrosky@gmail.com>
2021-09-13 12:30:43 -0500

    adding prepare script

---
+++ prepare.py
@@ -0,0 +1,62 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+from math import sqrt
+from scipy import stats
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text
+from sklearn.model_selection import train_test_split
+import sklearn.metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+############################# Function File for Preparing Data ############################
+
+
+def strip_strings(df):
+    '''
+    This function is designed to take in a dataframe and strip the trailing and leading whitespace
+    from the columns with string values in case they are there becuase this will cause issues in
+    exploration and modeling.
+    '''
+    
+    ## stripping trailing and leading whitespace from string valued columns just in case
+
+    df.name = df.name.str.strip()
+    df.country = df.country.str.strip()
+    
+    return df
+
+
+def split_data(df):
+    '''
+    This function is designed to split out data for modeling into train, validate, and test 
+    dataframes.
+    
+    It will also perform quality assurance checks on each dataframe to make sure the target 
+    variable was correctcly stratified into each dataframe.
+    '''
+    
+    ## splitting the data stratifying for out target variable is_fraud
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                            random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                       random_state=123)
+    
+    print('Making Sure Our Shapes Look Good')
+    print(f'Train: {train.shape}, Validate: {validate.shape}, Test: {test.shape}')
+    
+    return train, validate, test
+
